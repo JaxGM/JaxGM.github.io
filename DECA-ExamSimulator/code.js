795a8b98d1001bcc192605fd620dd07c8d9655dd
JaxGM <flume_prowl_0n@icloud.com>
2025-06-07 16:12:54 +0000

    Update from https://github.com/JaxGM/DECA-ExamSimulator/commit/d8f06c3d135f9db639492055ff08184ba6b1396a

--- DECA-ExamSimulator/code.js
+++ DECA-ExamSimulator/code.js
@@ -9,6 +9,8 @@ function onStart() {
         .then(response => console.log(response))
         .catch(err => console.error(err));
 
+    
+
 }
 
 function between(startStr, endStr, str) {
@@ -26,11 +28,12 @@ function dates() {
     // icdcDate = Math.round(Math.ceil(icdcDate - new Date())/(1000 * 3600 * 24));
 
     // Push
-    document.getElementById("dates").innerHTML = "<strong>"+cdcDate+"</strong>&nbsp;days&nbsp;until&nbsp;Flordia&nbsp;CDC&nbsp;| <strong>"+icdcDate+"</strong>&nbsp;days&nbsp;until&nbsp;ICDC";
+    document.getElementById("dates").innerHTML = "<strong>"+cdcDate+"</strong>&nbsp;days&nbsp;until&nbsp;Flordia&nbsp;CDC&nbsp;Testing&nbsp;| <strong>"+icdcDate+"</strong>&nbsp;days&nbsp;until&nbsp;ICDC";
 }
 
 function newExam(type) {
     url = document.getElementById("URL").value;
+    url = "https://cdn.prod.website-files.com/635c470cc81318fc3e9c1e0e/67c1d65441573664321a854f_24-25_BA%20Core%20Exam.pdf";
     console.log(url);
 
     loading = true;
@@ -63,29 +66,29 @@ function newExam(type) {
                     // A
                     questionSet = questionSet + "<div id=\"AnswerChoice\">"
                     questionSet = questionSet + "<input type=\"radio\" name=\"RadioButton"+i+"\" id=\"AButton\"></input>"
-                    questionSet = questionSet + "<p id=\"ALetter\">&nbspA.&nbsp</p>"
-                    questionSet = questionSet + "<p id=\"AText\">"+data[i][2]+"</p>"
+                    questionSet = questionSet + "<p id=\""+i+"ALetter\">&nbspA.&nbsp</p>"
+                    questionSet = questionSet + "<p id=\""+i+"AText\">"+data[i][2]+"</p>"
                     questionSet = questionSet + "</div>"
 
                     // B
                     questionSet = questionSet + "<div id=\"AnswerChoice\">"
                     questionSet = questionSet + "<input type=\"radio\" name=\"RadioButton"+i+"\" id=\"BButton\"></input>"
-                    questionSet = questionSet + "<p id=\"BLetter\">&nbspB.&nbsp</p>"
-                    questionSet = questionSet + "<p id=\"BText\">"+data[i][3]+"</p>"
+                    questionSet = questionSet + "<p id=\""+i+"BLetter\">&nbspB.&nbsp</p>"
+                    questionSet = questionSet + "<p id=\""+i+"BText\">"+data[i][3]+"</p>"
                     questionSet = questionSet + "</div>"
 
                     // C
                     questionSet = questionSet + "<div id=\"AnswerChoice\">"
                     questionSet = questionSet + "<input type=\"radio\" name=\"RadioButton"+i+"\" id=\"CButton\"></input>"
-                    questionSet = questionSet + "<p id=\"CLetter\">&nbspC.&nbsp</p>"
-                    questionSet = questionSet + "<p id=\"CText\">"+data[i][4]+"</p>"
+                    questionSet = questionSet + "<p id=\""+i+"CLetter\">&nbspC.&nbsp</p>"
+                    questionSet = questionSet + "<p id=\""+i+"CText\">"+data[i][4]+"</p>"
                     questionSet = questionSet + "</div>"
 
                     // D
                     questionSet = questionSet + "<div id=\"AnswerChoice\">"
                     questionSet = questionSet + "<input type=\"radio\" name=\"RadioButton"+i+"\" id=\"DButton\"></input>"
-                    questionSet = questionSet + "<p id=\"DLetter\">&nbspD.&nbsp</p>"
-                    questionSet = questionSet + "<p id=\"DText\">"+data[i][5]+"</p>"
+                    questionSet = questionSet + "<p id=\""+i+"DLetter\">&nbspD.&nbsp</p>"
+                    questionSet = questionSet + "<p id=\""+i+"DText\">"+data[i][5]+"</p>"
                     questionSet = questionSet + "</div>"
 
                     questionSet = questionSet + "</div>"
@@ -102,26 +105,26 @@ function newExam(type) {
 
                     // A
                     reviewSet = reviewSet + "<div id=\"ReviewChoice\">"
-                    reviewSet = reviewSet + "<p id=\"ALetter\">&nbspA.&nbsp</p>"
-                    reviewSet = reviewSet + "<p id=\"AText\">"+data[i][2]+"</p>"
+                    reviewSet = reviewSet + "<p id=\""+i+"ALetterR\">&nbspA.&nbsp</p>"
+                    reviewSet = reviewSet + "<p id=\""+i+"ATextR\">"+data[i][2]+"</p>"
                     reviewSet = reviewSet + "</div>"
 
                     // B
                     reviewSet = reviewSet + "<div id=\"ReviewChoice\">"
-                    reviewSet = reviewSet + "<p id=\"BLetter\">&nbspB.&nbsp</p>"
-                    reviewSet = reviewSet + "<p id=\"BText\">"+data[i][3]+"</p>"
+                    reviewSet = reviewSet + "<p id=\""+i+"BLetterR\">&nbspB.&nbsp</p>"
+                    reviewSet = reviewSet + "<p id=\""+i+"BTextR\">"+data[i][3]+"</p>"
                     reviewSet = reviewSet + "</div>"
 
                     // C
                     reviewSet = reviewSet + "<div id=\"ReviewChoice\">"
-                    reviewSet = reviewSet + "<p id=\"CLetter\">&nbspC.&nbsp</p>"
-                    reviewSet = reviewSet + "<p id=\"CText\">"+data[i][4]+"</p>"
+                    reviewSet = reviewSet + "<p id=\""+i+"CLetterR\">&nbspC.&nbsp</p>"
+                    reviewSet = reviewSet + "<p id=\""+i+"CTextR\">"+data[i][4]+"</p>"
                     reviewSet = reviewSet + "</div>"
 
                     // D
                     reviewSet = reviewSet + "<div id=\"ReviewChoice\">"
-                    reviewSet = reviewSet + "<p id=\"DLetter\">&nbspD.&nbsp</p>"
-                    reviewSet = reviewSet + "<p id=\"DText\">"+data[i][5]+"</p>"
+                    reviewSet = reviewSet + "<p id=\""+i+"DLetterR\">&nbspD.&nbsp</p>"
+                    reviewSet = reviewSet + "<p id=\""+i+"DTextR\">"+data[i][5]+"</p>"
                     reviewSet = reviewSet + "</div>"
 
                     reviewSet = reviewSet + "</div>"
@@ -156,8 +159,15 @@ function updateProgressPercent() {
 }
 
 function callQuestion(number) {
+    document.getElementById("advancement<").hidden = false;
+    document.getElementById("advancement>").hidden = false;
     document.getElementById("BubbleQuestion").hidden = false;
-    //hide rev
+
+    document.getElementById("BubbleReveiw").hidden = true;
+    document.getElementById("reviewButton").hidden = false;
+    document.getElementById("subScore").hidden = true;
+
+    
     console.log("calling" + number);
     document.getElementById("id"+currentQ).hidden = true;
     currentQ = number;
@@ -182,8 +192,51 @@ function callReview() {
 }
 
 function nextQuestion() {
-    //hide review
-    //restore controls
+    //commit answer to data
+    qualifyingButtons = document.getElementsByName('RadioButton' + currentQ);
+    for (i = 0; i < qualifyingButtons.length; i++) {
+        if (qualifyingButtons[i].checked) {
+            data[currentQ][8] = true;
+            data[currentQ][9] = qualifyingButtons[i].id.substring(0,1);
+            console.log(qualifyingButtons[i].id.substring(0,1))
+
+        }
+    }
+
+    //bold answer on review
+    console.log(currentQ+"CLetterR")
+    if ((data[currentQ][9] == "A") && data[currentQ][8]) {
+        document.getElementById(currentQ+"ALetterR").innerHTML = "<strong>" + "&nbspA.&nbsp" + "</strong>"
+        document.getElementById(currentQ+"ATextR").innerHTML = "<strong>" + data[currentQ][2] + "</strong>"
+    } else {
+        document.getElementById(currentQ+"ALetterR").innerHTML = "&nbspA.&nbsp"
+        document.getElementById(currentQ+"ATextR").innerHTML = data[currentQ][2]
+    }
+    if ((data[currentQ][9] == "B") && data[currentQ][8]) {
+        document.getElementById(currentQ+"BLetterR").innerHTML = "<strong>" + "&nbspB.&nbsp" + "</strong>"
+        document.getElementById(currentQ+"BTextR").innerHTML = "<strong>" + data[currentQ][3] + "</strong>"
+    } else {
+        document.getElementById(currentQ+"BLetterR").innerHTML = "&nbspB.&nbsp"
+        document.getElementById(currentQ+"BTextR").innerHTML = data[currentQ][3]
+    }
+    if ((data[currentQ][9] == "C") && data[currentQ][8]) {
+        document.getElementById(currentQ+"CLetterR").innerHTML = "<strong>" + "&nbspC.&nbsp" + "</strong>"
+        document.getElementById(currentQ+"CTextR").innerHTML = "<strong>" + data[currentQ][4] + "</strong>"
+    } else {
+        document.getElementById(currentQ+"CLetterR").innerHTML = "&nbspC.&nbsp"
+        document.getElementById(currentQ+"CTextR").innerHTML = data[currentQ][4]
+    }
+    if ((data[currentQ][9] == "D") && data[currentQ][8]) {
+        document.getElementById(currentQ+"DLetterR").innerHTML = "<strong>" + "&nbspD.&nbsp" + "</strong>"
+        document.getElementById(currentQ+"DTextR").innerHTML = "<strong>" + data[currentQ][5] + "</strong>"
+    } else {
+        document.getElementById(currentQ+"DLetterR").innerHTML = "&nbspD.&nbsp"
+        document.getElementById(currentQ+"DTextR").innerHTML = data[currentQ][5]
+    }
+
+    
+
+
 
 
     if (currentQ<100) {
@@ -192,11 +245,13 @@ function nextQuestion() {
         callReview();
     }
 }
+
 function lastQuestion() {
     if (currentQ!=1) {
         callQuestion (currentQ-1);
     }
 }
+
 function scoreTest () {
 
 }
